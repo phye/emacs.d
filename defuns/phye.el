bd2e1a2ae483e198fa4919690f58d14253e950cc
Philip Ye <phye8080@gmail.com>
2018-05-04 15:29:11 +0800

    Cleanup my TODO items

--- defuns/phye.el
+++ defuns/phye.el
@@ -60,6 +60,11 @@
 
 
 ;; {{ Org Mode
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "STARTED(s!)" "WAITING(w)" "BLOCKED(b@)" "|" "DONE(d)" "CANCELLED(c@)") ;; general todo items
+        (sequence "ASSIGNED(a!)" "STARTED(s!)" "REPRODUCED(p@)" "RCFOUND(r@)" "|" "FIXED(f)" "VERIFIED(v!)") ;; bug only
+        (type "APPT(a)" "REMINDER(r!)" "|" "DONE(d)"))) ;; misc daily items
+
 (setq org-agenda-files
      (quote
       ("~/ws/OrgNotes/gtd.org"
