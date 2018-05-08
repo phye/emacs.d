d65104d8d1932f249afa2f91e7bd541663a762c6
Philip Ye <phye8080@gmail.com>
2018-05-08 11:23:20 +0800

    Add a (not working) hook function after checkbox

--- defuns/phye.el
+++ defuns/phye.el
@@ -190,4 +190,26 @@
   (insert "#+setupfile: ~/.emacs.d/misc/include.org\n"))
 
 (define-auto-insert "\\.org$" #'phye/org-template)
+
+;; see http://thread.gmane.org/gmane.emacs.orgmode/42715
+(eval-after-load 'org-list
+  '(add-hook 'org-checkbox-statistics-hook (function ndk/checkbox-list-complete)))
+
+(defun ndk/checkbox-list-complete ()
+  (save-excursion
+    (org-back-to-heading t)
+    (let ((beg (point)) end)
+      (end-of-line)
+      (setq end (point))
+      (goto-char beg)
+      (if (re-search-forward "\\[\\([0-9]*%\\)\\]\\|\\[\\([0-9]*\\)/\\([0-9]*\\)\\]" end t)
+            (if (match-end 1)
+                (if (equal (match-string 1) "100%")
+                    ;; all done - do the state change
+                    (org-todo 'done)
+                  (org-todo 'todo))
+              (if (and (> (match-end 2) (match-beginning 2))
+                       (equal (match-string 2) (match-string 3)))
+                  (org-todo 'done)
+                (org-todo 'todo)))))))
 ;; }}
