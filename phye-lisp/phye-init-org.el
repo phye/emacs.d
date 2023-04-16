32ae20aa141c5c365d8876cdbc1c7795697b8bcd
Philip Ye <phye@tencent.com>
2023-04-16 20:47:11 +0800

    faster orgmode settings

--- phye-lisp/phye-init-org.el
+++ phye-lisp/phye-init-org.el
@@ -1,126 +1,73 @@
-;; {{ hooks
-(defun phye/org-mode-hook ()
-  "custom orgmode settings"
-  (interactive)
-  (set-fill-column 90)
-  (turn-on-auto-fill)
-  (org-clock-persistence-insinuate)
-  (hl-todo-mode 1)
-  (setq org-confirm-babel-evaluate nil)
-  (setq org-babel-default-header-args:plantuml
-        '((:results . "replace")
-          (:exports . "results")))
-  (setq org-tags-column -80)
-  (setq org-catch-invisible-edits (quote error))
-  (setq safe-local-variable-values (quote ((lentic-init . lentic-orgel-org-init))))
-  (setq org-deadline-warning-days 7)
-  (setq org-log-into-drawer t)
-  (setq org-clock-persist 'history)
-  (setq org-src-window-setup 'split-window-below)
-  (setq org-archive-location "archive.org.gpg::datetree/* From %s")
-  (setq org-adapt-indentation t)
-  (define-key org-mode-map (kbd "C-c o") 'org-open-at-point)
-  (add-to-list 'org-src-lang-modes '("plantuml" . plantuml))
-  (my-yas-reload-all))
-(add-hook 'org-mode-hook 'phye/org-mode-hook 90)
-;; }}
+;; general editting
+(setq org-tags-column -80)
+(setq org-catch-invisible-edits (quote error))
+(setq org-adapt-indentation t)
 
-;; {{ Complex Org settings
-(with-eval-after-load 'org
-  (setq org-tag-alist '((:startgroup . nil) ;; tag group for address
-                        ("@work" . ?w)
-                        ("@home" . ?h)
-                        ("@travel" . ?t)
-                        (:endgroup . nil)
-                        (:startgroup . nil) ;; tag group for reading
-                        ("book" . ?b)
-                        ("kindle" . ?k)
-                        ("pad" . ?d)
-                        ("computer" . ?c)
-                        (:endgroup . nil)
-                        (:startgroup . nil) ;; tag group for privacy
-                        ("personal" . ?p)
-                        ("office" . ?o)
-                        ("public" . ?a)
-                        (:endgroup . nil)
-                        (:startgroup . nil) ;; tag group for kind of things
-                        ("project" . ?j)
-                        ("reading" . ?r)
-                        ("ideas" . ?i)
-                        ("misc" . ?m)
-                        ("financial" . ?f)
-                        ("health" . ?h)
-                        ("gtd" . ?g)
-                        (:endgroup)))
+;; capture, refile and archive
+(setq org-refile-targets
+      '((nil :maxlevel . 5)
+        (org-agenda-files :maxlevel . 5)
+        ("KnowledgeBase.org" :maxlevel . 5)
+        ("done.org" :maxlevel . 5)))
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "~/ws/gtd/gtd.org" "Tasks")
+         "* TODO %?\n %i\n %a")
+        ("n" "Note" entry (file+datetree "~/ws/gtd/quick_notes.org")
+         "* %?\nEntered on %U\n %i\n %a")
+        ("j" "Journal entry" entry (function org-journal-find-location)
+         "* %(format-time-string org-journal-time-format)%^{Title}\n%i%?")))
+(setq org-archive-location "archive.org.gpg::datetree/* From %s")
 
-  (setq org-todo-keywords
-        '((sequence "TODO(t!/!)" "SCHEDULED(S@/@)" "INPROGRESS(i!/!)" "BLOCKED(b@/@)" "|" "DONE(d)" "DEFERED(f@/@)" "CANCELLED(c@/!)") ;; general todo items
-          (sequence "ASSIGNED(a@/!)" "REPRODUCED(p@)" "RCFOUND(r@)" "|" "FIXED(x!)" "VERIFIED(v!)") ;; bug only
-          (type "APPT(p)" "REMINDER(m!)" "|" "DONE(d)"))) ;; misc daily items
+;; org-babel (src)
+(setq org-src-window-setup 'split-window-below)
+(setq org-confirm-babel-evaluate nil)
+(setq org-babel-default-header-args:plantuml
+      '((:results . "replace")
+        (:exports . "results")))
 
-  (setq org-agenda-files
-        (quote
-         ("~/ws/gtd/gtd.org"
-          "~/ws/gtd/quick_notes.org"
-          "~/ws/gtd/life/plan_life.org"
-          "~/ws/gtd/life/birthday.org"
-          "~/ws/gtd/learn/plan_learning.org"
-          "~/ws/gtd/work/plan_work.org.gpg")))
+;; gtd status related
+(setq org-log-into-drawer t)
+(setq org-tag-alist '((:startgroup . nil) ;; tag group for address
+                      ("@work" . ?w)
+                      ("@home" . ?h)
+                      ("@travel" . ?t)
+                      (:endgroup . nil)
+                      (:startgroup . nil) ;; tag group for reading
+                      ("book" . ?b)
+                      ("kindle" . ?k)
+                      ("pad" . ?d)
+                      ("computer" . ?c)
+                      (:endgroup . nil)
+                      (:startgroup . nil) ;; tag group for privacy
+                      ("personal" . ?p)
+                      ("office" . ?o)
+                      ("public" . ?a)
+                      (:endgroup . nil)
+                      (:startgroup . nil) ;; tag group for kind of things
+                      ("project" . ?j)
+                      ("reading" . ?r)
+                      ("ideas" . ?i)
+                      ("misc" . ?m)
+                      ("financial" . ?f)
+                      ("health" . ?h)
+                      ("gtd" . ?g)
+                      (:endgroup)))
+(setq org-todo-keywords
+      '((sequence "TODO(t!/!)" "SCHEDULED(S@/@)" "INPROGRESS(i!/!)" "BLOCKED(b@/@)" "|" "DONE(d)" "DEFERED(f@/@)" "CANCELLED(c@/!)") ;; general todo items
+        (sequence "ASSIGNED(a@/!)" "REPRODUCED(p@)" "RCFOUND(r@)" "|" "FIXED(x!)" "VERIFIED(v!)") ;; bug only
+        (type "APPT(p)" "REMINDER(m!)" "|" "DONE(d)"))) ;; misc daily items
 
-  (setq org-refile-targets
-        '((nil :maxlevel . 5)
-          (org-agenda-files :maxlevel . 5)
-          ("KnowledgeBase.org" :maxlevel . 5)
-          ("done.org" :maxlevel . 5)))
-
-  ;; org babel
-  (org-babel-do-load-languages
-   'org-babel-load-languages
-   '(
-     (ditaa . t)
-     (plantuml . t)
-     (dot . t)
-     ))
-
-  ;; Org Mode Capture
-  (setq org-capture-templates
-        '(("t" "Todo" entry (file+headline "~/ws/gtd/gtd.org" "Tasks")
-           "* TODO %?\n %i\n %a")
-          ("n" "Note" entry (file+datetree "~/ws/gtd/quick_notes.org")
-           "* %?\nEntered on %U\n %i\n %a")
-          ("j" "Journal entry" entry (function org-journal-find-location)
-           "* %(format-time-string org-journal-time-format)%^{Title}\n%i%?")))
-
-  ;; OrgMode Output
-  (require 'ox-md nil t)
-  (require 'ox-odt nil t)
-  (setq org-export-with-sub-superscripts nil)
-  (setq org-export-with-properties t)
-
-  (with-eval-after-load 'ox-latex
-    ;; update the list of LaTeX classes and associated header (encoding, etc.)
-    ;; and structure
-    (add-to-list 'org-latex-classes
-                 `("beamer"
-                   ,(concat "\\documentclass[presentation]{beamer}\n"
-                            "[DEFAULT-PACKAGES]"
-                            "[PACKAGES]"
-                            "[EXTRA]\n")
-                   ("\\section{%s}" . "\\section*{%s}")
-                   ("\\subsection{%s}" . "\\subsection*{%s}")
-                   ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))))
-
-  (setq org-latex-listings t)
-  (setq org-latex-to-pdf-process
-        '("xelatex -interaction nonstopmode -output-directory %o %f"
-          "xelatex -interaction nonstopmode -output-directory %o %f"
-          "xelatex -interaction nonstopmode -output-directory %o %f")))
-
-;; }} Complex Org Settings
-
-;; {{ Org Mode Extensions
-
-;; {{ calendar and org-agenda
+;; gtd time related
+(setq org-deadline-warning-days 7)
+(setq org-clock-persist 'history)
+(setq org-agenda-files
+      (quote
+       ("~/ws/gtd/gtd.org"
+        "~/ws/gtd/quick_notes.org"
+        "~/ws/gtd/life/plan_life.org"
+        "~/ws/gtd/life/birthday.org"
+        "~/ws/gtd/learn/plan_learning.org"
+        "~/ws/gtd/work/plan_work.org.gpg")))
 (customize-set-variable 'holiday-local-holidays
                         '((holiday-fixed 1 1 "元旦")
                           (holiday-fixed 3 8 "妇女节")
@@ -131,7 +78,6 @@
                           (holiday-fixed 8 1 "建军节")
                           (holiday-fixed 10 1 "国庆节")
                           ))
-
 (with-eval-after-load 'org-agenda
   (define-key org-agenda-mode-map (kbd "W") 'org-agenda-week-view)
   (define-key org-agenda-mode-map (kbd "F") 'org-agenda-fortnight-view)
@@ -145,7 +91,50 @@
 (with-eval-after-load 'ox-icalendar
   (customize-set-variable 'org-icalendar-combined-name "OrgMode")
   (customize-set-variable 'org-icalendar-combined-agenda-file "~/ws/gtd/gtd.ics"))
-;; }}
+
+;; output related
+(setq org-export-with-sub-superscripts nil)
+(setq org-export-with-properties t)
+
+;; output/latex
+(with-eval-after-load 'ox-latex
+  ;; update the list of LaTeX classes and associated header (encoding, etc.)
+  ;; and structure
+  (add-to-list 'org-latex-classes
+               `("beamer"
+                 ,(concat "\\documentclass[presentation]{beamer}\n"
+                          "[DEFAULT-PACKAGES]"
+                          "[PACKAGES]"
+                          "[EXTRA]\n")
+                 ("\\section{%s}" . "\\section*{%s}")
+                 ("\\subsection{%s}" . "\\subsection*{%s}")
+                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))))
+(setq org-latex-listings t)
+(setq org-latex-to-pdf-process
+      '("xelatex -interaction nonstopmode -output-directory %o %f"
+        "xelatex -interaction nonstopmode -output-directory %o %f"
+        "xelatex -interaction nonstopmode -output-directory %o %f"))
+
+;; {{ settings after org loaded
+(with-eval-after-load 'org
+  (my-run-with-idle-timer 1
+   (lambda ()
+     ;; org babel
+     (my-yas-reload-all)
+     (add-to-list 'org-src-lang-modes '("plantuml" . plantuml))
+     (define-key org-mode-map (kbd "C-c o") 'org-open-at-point))
+     (org-babel-do-load-languages
+      'org-babel-load-languages
+      '(
+        (ditaa . t)
+        (plantuml . t)
+        (dot . t)
+        ))
+     (require 'ox-md nil t)
+     (require 'ox-odt nil t)))
+  )
+
+;; {{ Org Mode Extensions
 
 ;; {{ org-journal related
 (use-package org-journal
@@ -158,7 +147,6 @@
   (customize-set-variable 'org-journal-date-format "%A, %x")
   (org-journal-file-format "%Y%m.org")
   (org-journal-file-type 'monthly))
-
 ;; org-journal capture
 ;; Refer to https://github.com/bastibe/org-journal
 (defun org-journal-find-location ()
@@ -168,7 +156,6 @@
   ;; Position point on the journal's top-level heading so that org-capture
   ;; will add the new entry as a child entry.
   (goto-char (point-min)))
-
 (defun org-journal-delete ()
   ;; Delete current journal file and buffer
   (interactive)
@@ -220,6 +207,7 @@
   (insert "#+title: \n")
   (insert "#+setupfile: ~/.emacs.d/misc/include.org\n"))
 (define-auto-insert "\\.org$" #'phye/org-template)
+
 ;; see http://thread.gmane.org/gmane.emacs.orgmode/42715
 (with-eval-after-load 'org-list
   '(add-hook 'org-checkbox-statistics-hook (function ndk/checkbox-list-complete)))
@@ -240,7 +228,18 @@
                        (equal (match-string 2) (match-string 3)))
                   (org-todo 'done)
                 (org-todo 'todo)))))))
+;; }}
 
+;; {{ hooks
+(defun phye/org-mode-hook ()
+  "custom orgmode settings"
+  (interactive)
+  (set-fill-column 90)
+  (turn-on-auto-fill)
+  (org-clock-persistence-insinuate)
+  (hl-todo-mode 1)
+  ;; (setq safe-local-variable-values (quote ((lentic-init . lentic-orgel-org-init))))
+(add-hook 'org-mode-hook 'phye/org-mode-hook 90)
 ;; }}
 
 (provide 'phye-init-org)
