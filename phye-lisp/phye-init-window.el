db10a254a5862dcfb6e490656f65483da99fea4a
Philip Ye <phye@tencent.com>
2023-05-19 19:15:47 +0800

    log frame name on switch

--- phye-lisp/phye-init-window.el
+++ phye-lisp/phye-init-window.el
@@ -103,7 +103,6 @@
   (interactive)
   (let ((name (substring-no-properties
                (cdr (assoc 'name (frame-parameters))))))
-    (message "Frame Name: %s" name)
     name))
 
 (defvar phye/last-frame-name (phye/current-frame-name))
@@ -114,7 +113,8 @@
   (unless (string= (phye/current-frame-name) phye/frame-name-before-switch)
     (setq phye/frame-name-after-switch (phye/current-frame-name))
     (setq phye/last-frame-name phye/frame-name-before-switch)
-    (setq phye/frame-name-before-switch phye/frame-name-after-switch)))
+    (setq phye/frame-name-before-switch phye/frame-name-after-switch)
+    (message "Switch frame from %s to %s" phye/last-frame-name phye/frame-name-after-switch)))
 (add-hook 'buffer-list-update-hook #'phye/buffer-list-update-hook)
 
 (defun phye/toggle-last-frame ()
