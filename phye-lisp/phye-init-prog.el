44c21fb30188d246c651350c27efa7ac9221c354
Philip Ye <phye.pro@gmail.com>
2024-03-18 19:21:19 +0800

    cleanup prog settings

--- phye-lisp/phye-init-prog.el
+++ phye-lisp/phye-init-prog.el
@@ -7,7 +7,8 @@
   :ensure t
   :defer t
   :config
-  (setq shell-pop-shell-type '("ansi-term" "*ansi-term*" (lambda nil (ansi-term shell-pop-term-shell))))
+  (setq shell-pop-shell-type
+        '("ansi-term" "*ansi-term*" (lambda nil (ansi-term shell-pop-term-shell))))
   (setq shell-pop-term-shell zsh-program)
   ;; need to do this manually or not picked up by `shell-pop'
   (shell-pop--set-shell-type 'shell-pop-shell-type shell-pop-shell-type))
@@ -32,7 +33,6 @@
 ;;   (cons 'company-capf company-backends)
 ;;   (setq company-backends (cl-remove-duplicates company-backends))) ; start autocompletion only after typin)
 
-
 (setq phye/general-ignore-directories
   '(
     "build"
@@ -42,19 +42,26 @@
     "cache"
     ;; "model"
     "lib"
-    "third_path"
-    "cc_tool"
-    "netcapture/proto"
-    "crm_client/dm_nlp_svrs/nlp_structured_msg_svr/client/proto"
-    ))
+    "third_path"))
 
 ;; counsel
 (with-eval-after-load 'counsel-etags
   (setq counsel-etags-debug t)
-  (setq counsel-etags-ignore-directories (append phye/general-ignore-directories counsel-etags-ignore-directories))
+  (setq counsel-etags-ignore-directories
+        (append phye/general-ignore-directories counsel-etags-ignore-directories))
   (add-to-list 'counsel-etags-ignore-filenames "*_pb2.py")
   (add-to-list 'counsel-etags-ignore-filenames "*.pb.h")
   (add-to-list 'counsel-etags-ignore-filenames "*.pb.cc"))
+
+(with-eval-after-load 'find-file-in-project
+  (add-to-list 'ffip-prune-patterns "*/build")
+  (add-to-list 'ffip-prune-patterns "*/rpm_build")
+  (add-to-list 'ffip-prune-patterns "*/cc_tool")
+  (add-to-list 'ffip-prune-patterns "*/qci_files")
+  (add-to-list 'ffip-prune-patterns "*/vendor")
+  (add-to-list 'ffip-ignore-filenames "*.pb.cc")
+  (add-to-list 'ffip-ignore-filenames "*.pb.h")
+  (add-to-list 'ffip-ignore-filenames "*_pb2.py"))
 
 ;; ediff
 (defvar previous-theme nil "previous theme before ediff for backup")
@@ -67,22 +74,6 @@
 (add-hook 'ediff-startup-hook #'phye/ediff-startup-hook)
 (add-hook 'ediff-cleanup-hook #'phye/ediff-cleanup-hook)
 
-;; projectile-mode, multiple projects
-;; (use-package projectile
-;;   :ensure t
-;;   :defer t
-;;   ;; :bind (("C-c x" . projectile-command-map))
-;;   :config
-;;   (projectile-mode +1)
-;;   (define-key projectile-mode-map (kbd "C-c x") 'projectile-command-map)
-;;   (setq projectile-indexing-method 'hybrid)
-;;   (setq projectile-globally-ignored-directories
-;;         (append phye/general-ignore-directories projectile-globally-ignored-directories))
-;;   (add-to-list 'projectile-globally-ignored-file-suffixes "pb.cc")
-;;   (add-to-list 'projectile-globally-ignored-file-suffixes "pb.h")
-;;   (add-to-list 'projectile-globally-ignored-file-suffixes "py")
-;;   (add-to-list 'projectile-globally-ignored-file-suffixes "pyc"))
-
 ;; log
 (defun phye/view-log-with-color ()
   (interactive)
@@ -90,22 +81,6 @@
 
 ;; python
 (setq elpy-rpc-python-command (string-trim (shell-command-to-string "which python3")))
-
-(with-eval-after-load 'find-file-in-project
-  (add-to-list 'ffip-prune-patterns "*/build")
-  (add-to-list 'ffip-prune-patterns "*/rpm_build")
-  (add-to-list 'ffip-prune-patterns "*/cc_tool")
-  (add-to-list 'ffip-prune-patterns "*/qci_files")
-  (add-to-list 'ffip-prune-patterns "*/vendor")
-  (add-to-list 'ffip-ignore-filenames "*.pb.cc")
-  (add-to-list 'ffip-ignore-filenames "*.pb.h")
-  (add-to-list 'ffip-ignore-filenames "*_pb2.py")
-  )
-
-(with-eval-after-load 'rg
-  (define-key rg-mode-map (kbd "n") 'rg-next-file)
-  (define-key rg-mode-map (kbd "p") 'rg-prev-file)
-  )
 
 (use-package tree-sitter
   :ensure t
