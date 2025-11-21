a47e81315d81268dbcb42f27bcb0fe8e2a0ad5a5
Philip Ye <phye@tencent.com>
2025-11-21 16:14:30 +0800

    fix comment

--- phye-lisp/phye-init-git.el
+++ phye-lisp/phye-init-git.el
@@ -3,6 +3,10 @@
 ;; use magit to edit commit message
 (require 'magit)
 
+;; To use this function:
+;; 1. Select a region containing HTTPS Git URLs.
+;; 2. M-x convert-https-to-ssh-in-region
+
 (defun convert-https-to-ssh-in-region (start end)
   "Convert HTTPS Git URLs to SSH format in the selected region."
   (interactive "r")
@@ -12,9 +16,5 @@
       (let ((domain (match-string 1))
             (repo-path (match-string 2)))
         (replace-match (format "git@%s:%s" domain repo-path) t t)))))
-
-;; To use this function:
-;; 1. Select a region containing HTTPS Git URLs.
-;; 2. M-x convert-https-to-ssh-in-region
 
 (provide 'phye-init-git)
